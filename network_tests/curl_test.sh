a37eccbddfd2c56f91114ea7fe132b8d1310e54a
John Carden <john.carden@gmail.com>
2023-08-02 12:45:57 -0400

    updates to curl requests

--- network_tests/curl_test.sh
+++ network_tests/curl_test.sh
@@ -5,6 +5,10 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+# Check curl version
+curl_version=$(curl --version 2>&1 | head -n 1 | awk '{ print $2 }')
+echo "curl version is $curl_version, the minimum required version for error message output is 7.75.0"
+
 declare -A endpoints
 
 # VCenter and ESXi hosts on 443
@@ -21,8 +25,12 @@ endpoints["nsx-alb-controller1"]="172.16.10.41:8443:22:443"
 # DNS Server on 53
 endpoints["dns"]="192.168.4.2:53"
 
-# frontend network endpoints 
+# kube-api-server endpoints 
 endpoints["kube-api-server"]="172.16.102.51:6443"
+
+# litmus test
+endpoints["sivt-management"]="172.16.10.28:443:6443"
+endpoints["sivt-frontend"]="172.16.102.235:8443:22:443:6443"
 
 # ntp server test
 ntp_server="192.168.4.2"
@@ -71,13 +79,15 @@ for key in ${!endpoints[@]}; do
     n=${#value_array[@]} # getting length of array
     for ((i = 1; i < n; i++)); do
         echo -n "Testing ${value_array[0]}:${value_array[$i]} --> "
-        curl_result=$(curl -v "${value_array[0]}:${value_array[$i]}" 2>&1)
-        if [[ "$curl_result" == *"Connection refused"* ]]; then
-            echo -e "${RED}Connection Failed${NC}"
+        response=$(curl -s -o /dev/null -w "%{response_code}:%{errormsg}" "${value_array[0]}:${value_array[$i]}")
+        #echo "Response: $response"
+        IFS=":" read -ra response_array <<< "$response"
+
+        if [[ "${response_array[1]}" == *"Failed to connect"* ]]; then
+            echo -e "${RED}${response_array[0]} ${response_array[1]}${NC}"
         else
-            echo -e "${GREEN}OK${NC}"
+            echo -e "${GREEN}${response_array[0]} OK ${NC}${response_array[1]}"
         fi
-        #echo $curl_result
     done
     echo ""
 done
